d9ff73e63d9179e5c0ea56ef646cb2cc4d0841f6
melo5915 <116410196+melo5915@users.noreply.github.com>
2025-01-29 12:41:55 -0500

    This is the beginning of melosta5915's three hours of work. Created the shared git repository and added files.

---
+++ FileHandling.cpp
@@ -0,0 +1,98 @@
+// current file: FileHandling.cpp
+// project name: melosta5915_t_w3
+// programmer name: Mohamad-Aref El-Osta
+// current version: v01
+
+/*
+functionality:
+
+I am setting up the assignment methods 
+according to the lab instructions 
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "Net.h"
+
+
+using namespace std;
+using namespace net;
+
+
+class Files
+{
+
+	public:
+
+		//name: Files
+		//functionality: will be the constructor
+		//parameters: 
+		//return: 
+		Files() 
+		{
+
+		}
+
+
+		//name: LoadStoreFile
+		//functionality: will load and store files content
+		//parameters:
+		//return:
+		void LoadStoreFile()
+		{
+			
+
+		} 
+
+
+		//name: VerifyFileContents
+		//functionality: will verify the files contents
+		//parameters:
+		//return:
+		void VerifyFileContents()
+		{
+
+
+		}
+
+
+
+		// function name:SendFile
+		// functionality:This function will send the file
+		// parameters: void 
+		// return: void
+		void SendFile()
+		{
+
+		}
+
+
+
+		// function name:ReceiveFile
+		// functionality: This function will receive the file
+		// parameters: void
+		// return: void
+		void ReceiveFile()
+		{
+
+		}
+
+
+
+		//name: CalcTransTime
+		//functionality: will calculate transmission time of the file
+		//parameters:
+		//return:
+		void CalcTransTime()
+		{
+
+
+		}
+
+
+
+
+};
